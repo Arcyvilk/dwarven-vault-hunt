14844888f6207e6c5054daf770fec94f609392e5
Arcyvilk <blazemakerrr@gmail.com>
2025-01-02 17:21:49 +0100

    feat: 🎸 add a custom arcyscene class

---
+++ src/game/scenes/ArcyScene.ts
@@ -0,0 +1,224 @@
+import PhaserRaycaster from 'phaser-raycaster'
+import { Scene } from 'phaser';
+import { EventBus } from '../EventBus';
+
+export class ArcyScene extends Scene
+{
+    raycasterPlugin: PhaserRaycaster
+    raycaster: Raycaster
+    ray: Raycaster.Ray
+    rayIntersections: Phaser.Geom.Point[]
+    graphics: Phaser.GameObjects.Graphics
+    maskGraphics: Phaser.GameObjects.Graphics
+    mask: Phaser.Display.Masks.GeometryMask
+    fov: Phaser.GameObjects.Graphics
+
+    controls: any
+    camera: Phaser.Cameras.Scene2D.Camera;
+    background: Phaser.GameObjects.Image;
+    gameText: Phaser.GameObjects.Text;
+    player: Phaser.Types.Physics.Arcade.ImageWithDynamicBody
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys
+    playerLight: Phaser.GameObjects.Light
+
+    tileSize: number;
+    map: Phaser.Tilemaps.Tilemap
+    collisionLayers: Phaser.Tilemaps.TilemapLayer[]
+    rayCollisionLayers: Phaser.Tilemaps.TilemapLayer[]
+
+    constructor(sceneName: string) {
+        super(sceneName);
+        this.tileSize = 64
+    }
+
+    customPreload(tilemapName: string, tilemapPath: string) {
+        this.load.setBaseURL('https://cdn.arcyvilk.com/dwarven_grail_hunters')
+        this.load.tilemapTiledJSON(tilemapName, tilemapPath)
+        this.load.image('player', 'player.png')
+    }
+
+    customCreate(
+        tilemapName: string,
+        playerStartX: number,
+        playerStartY: number
+    ) {
+        this.map = this.make.tilemap({ key: tilemapName, tileWidth: this.tileSize, tileHeight: this.tileSize })
+        this.setBounds()
+        this.setControls()
+
+        this.setPlayer(playerStartX, playerStartY)
+        this.setPlayerMovement()
+
+        // load layers here
+
+        // raycasting after
+    }
+
+    customLightning() {
+        this.setLightningMask()
+        this.setRaycasting()
+    }
+
+    setBounds() {
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels * 2, this.map.heightInPixels * 2);
+        this.physics.world.setBounds(0, 0, this.map.widthInPixels * 2, this.map.heightInPixels * 2);
+    }
+
+    setControls() {
+        this.cursors = this.input.keyboard!.createCursorKeys();
+        const controlConfig = {
+            camera: this.cameras.main,
+            left: this.cursors.left,
+            right: this.cursors.right,
+            up: this.cursors.up,
+            down: this.cursors.down,
+            speed: this.tileSize
+        };
+        this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+    }
+
+    setPlayer(playerStartX: number, playerStartY: number) {
+        this.player = this.physics.add.image(playerStartX, playerStartY, 'player')
+        this.player.setCollideWorldBounds(true)
+        this.player.setDepth(5)
+
+        // Attach camera to player
+        this.cameras.main.setRoundPixels(true)
+        this.cameras.main.startFollow(this.player, true)
+    }
+
+    setLightningMask() {
+        this.graphics = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff,
+                alpha: 0.3
+            }
+        });
+        
+        // create FoV
+        this.maskGraphics = this.add.graphics({
+            fillStyle: {
+                color: 0xffffff,
+                alpha: 0
+            }
+        });
+        this.mask = new Phaser.Display.Masks.GeometryMask(this, this.maskGraphics);
+        this.mask.setInvertAlpha();
+        this.fov = this.add.graphics({
+            fillStyle: {
+                color: 0x000000,
+                alpha: 0.95,
+            }
+        }).setDepth(29);
+        this.fov.setMask(this.mask);
+        this.fov.fillRect(0, 0, this.map.widthInPixels * 2, this.map.heightInPixels * 2);
+
+        //set depths
+        this.fov.setDepth(1);
+        this.graphics.setDepth(3);
+
+        this.rayCollisionLayers.forEach(rayCollisionLayer => {
+            rayCollisionLayer.setDepth(1)
+        })
+    }
+
+    setRaycasting() {
+        this.raycaster = this.raycasterPlugin.createRaycaster({
+            autoUpdate: false,
+        });
+
+        this.rayCollisionLayers.forEach(rayCollisionLayer => {
+            this.raycaster.mapGameObjects(rayCollisionLayer, true, {
+                collisionTiles: rayCollisionLayer.layer.collideIndexes
+            });
+        })
+
+        this.ray = this.raycaster.createRay();
+        this.updateRays()
+    }
+
+    setPlayerMovement() {
+        this.input.keyboard!.on('keydown-LEFT', () => {
+            if (this.isTileCollider('left')) return;
+            this.player.x -= this.tileSize
+            this.updateRays()
+            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
+        })
+        this.input.keyboard!.on('keydown-RIGHT', () => {
+            if (this.isTileCollider('right')) return;
+            this.player.x += this.tileSize
+            this.updateRays()
+            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
+        })
+        this.input.keyboard!.on('keydown-UP', () => {
+            if (this.isTileCollider('up')) return;
+            this.player.y -= this.tileSize
+            this.updateRays()
+            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
+        })
+        this.input.keyboard!.on('keydown-DOWN', () => {
+            if (this.isTileCollider('down')) return;
+            this.player.y += this.tileSize
+            this.updateRays()
+            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
+        })
+    }
+
+    updateRays() {
+        this.ray.setOrigin(this.player.x, this.player.y)
+        this.rayIntersections = this.ray.castCircle()
+        this.drawRays()
+    }
+
+    drawRays() {
+        this.graphics.clear();
+        this.maskGraphics.clear();
+        this.maskGraphics.fillPoints(this.rayIntersections);
+
+        // draw rays
+        for (const intersection of this.rayIntersections) {
+            // @ts-ignore
+            this.graphics.strokeLineShape({
+                x1: this.ray.origin.x,
+                y1: this.ray.origin.y,
+                x2: intersection.x,
+                y2: intersection.y
+            });
+
+            this.graphics.fillStyle(0xff00ff);
+            //draw ray origin
+            this.graphics.fillPoint(this.ray.origin.x, this.ray.origin.y, 3);
+        }       
+    }
+
+    isTileCollider(direction: 'left'|'right'|'down'|'up') {
+        const getTilePosition = (): number[] => {
+            if (direction === 'left') {
+                return [this.player.x - this.tileSize, this.player.y]
+            }
+            if (direction === 'right') {
+                return [this.player.x + this.tileSize, this.player.y]
+            }
+            if (direction === 'down') {
+                return [this.player.x, this.player.y + this.tileSize]
+            }
+            if (direction === 'up') {
+                return [this.player.x, this.player.y - this.tileSize]
+            }
+            return [0,0]
+        }
+
+        // Get position of the tile where player will move to soon
+        const [x, y] = getTilePosition()
+
+        // Check if the tile is present in any of the "collision" layers
+        const collisionTiles = this.collisionLayers.map(layer => 
+            layer.getTileAtWorldXY(x, y, true)?.index ?? -1
+        )
+        
+        // If yes, the tile is impassable and we have to block player from moving through it
+        if (collisionTiles.some(index => index !== -1)) return true
+        return false
+    }
+}
+
--- src/game/scenes/Home/index.ts
+++ src/game/scenes/Home/index.ts
@@ -1,29 +1,7 @@
-import PhaserRaycaster from 'phaser-raycaster'
-import { Scene } from 'phaser';
-import { EventBus } from '../../EventBus';
+import { ArcyScene } from '../ArcyScene';
 
-export class Home extends Scene
+export class Home extends ArcyScene
 {
-    raycasterPlugin: PhaserRaycaster
-    raycaster: Raycaster
-    ray: Raycaster.Ray
-    rayIntersections: Phaser.Geom.Point[]
-    graphics: Phaser.GameObjects.Graphics
-    maskGraphics: Phaser.GameObjects.Graphics
-    mask: Phaser.Display.Masks.GeometryMask
-    fov: Phaser.GameObjects.Graphics
-
-    controls: any
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    gameText: Phaser.GameObjects.Text;
-    player: Phaser.Types.Physics.Arcade.ImageWithDynamicBody
-    cursors: Phaser.Types.Input.Keyboard.CursorKeys
-    playerLight: Phaser.GameObjects.Light
-
-    tileSize: number;
-
-    // Layers
     layerWalls: Phaser.Tilemaps.TilemapLayer
     layerFloor: Phaser.Tilemaps.TilemapLayer
     layerFurniture: Phaser.Tilemaps.TilemapLayer
@@ -31,17 +9,26 @@ export class Home extends Scene
     layerStockpiles: Phaser.Tilemaps.TilemapLayer
     layerDoor: Phaser.Tilemaps.TilemapLayer
 
-    constructor ()
-    {
+    constructor () {
         super('Home');
-        this.tileSize = 64
     }
 
     preload() {
-        this.load.setBaseURL('https://cdn.arcyvilk.com/dwarven_grail_hunters')
-        this.load.tilemapTiledJSON('map_home', 'map_home.json')
+        this.customPreload('map_home', 'map_home.json')
+        this.preloadLayerImages()
+    }
 
-        // Load environment sprites
+    create() {
+        this.customCreate('map_home', 1888, 736)
+        this.loadLayers(this.map)
+        this.customLightning()
+    }
+
+    update(/**time, delta */) {
+        // something
+    }
+
+    preloadLayerImages() {
         this.load.image('wall_wooden', "df_tilesets/wall_wooden.png")
         this.load.image('floor_stone_engraved_palette', "df_tilesets/floor_stone_engraved_palette.png")
         this.load.image('glass_floor', "df_tilesets/glass_floor.png")
@@ -53,162 +40,7 @@ export class Home extends Scene
         this.load.image('items3', "df_tilesets/items3.png")
         this.load.image('instruments', "df_tilesets/instruments.png")
         this.load.image('hidden_rock', "df_tilesets/hidden_rock.png")
-        // Load player sprite
-        this.load.image('player', 'player.png')
     }
-
-    create() {
-        const map = this.make.tilemap({ key: 'map_home', tileWidth: this.tileSize, tileHeight: this.tileSize })
-        this.loadLayers(map)
-        this.setBounds(map)
-        this.setControls()
-
-        this.setPlayer()
-        this.setPlayerMovement()
-
-        this.setLightningMask(map)
-        this.setRaycasting()
-    }
-
-    update(/**time, delta */) {
-        // something
-    }
-
-    setBounds(map: Phaser.Tilemaps.Tilemap) {
-        this.cameras.main.setBounds(0, 0, map.widthInPixels * 2, map.heightInPixels * 2);
-        this.physics.world.setBounds(0, 0, map.widthInPixels * 2, map.heightInPixels * 2);
-    }
-
-    setControls() {
-        this.cursors = this.input.keyboard!.createCursorKeys();
-        const controlConfig = {
-            camera: this.cameras.main,
-            left: this.cursors.left,
-            right: this.cursors.right,
-            up: this.cursors.up,
-            down: this.cursors.down,
-            speed: this.tileSize
-        };
-        this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-    }
-
-    setPlayer() {
-        // Create player sprite and place it in the world
-        const playerStartX = 1888 // 32 + this.tileSize * 10
-        const playerStartY = 736 // 32 + this.tileSize * 5
-        this.player = this.physics.add.image(playerStartX, playerStartY, 'player')
-        this.player.setCollideWorldBounds(true)
-        this.player.setDepth(5)
-
-        // Attach camera to player
-        this.cameras.main.setRoundPixels(true)
-        this.cameras.main.startFollow(this.player, true)
-    }
-
-    setLightningMask(map: Phaser.Tilemaps.Tilemap) {
-        this.graphics = this.add.graphics({
-            fillStyle: {
-                color: 0xffffff,
-                alpha: 0.3
-            }
-        });
-        
-        // create FoV
-        this.maskGraphics = this.add.graphics({
-            fillStyle: {
-                color: 0xffffff,
-                alpha: 0
-            }
-        });
-        this.mask = new Phaser.Display.Masks.GeometryMask(this, this.maskGraphics);
-        this.mask.setInvertAlpha();
-        this.fov = this.add.graphics({
-            fillStyle: {
-                color: 0x000000,
-                alpha: 0.95,
-            }
-        }).setDepth(29);
-        this.fov.setMask(this.mask);
-        this.fov.fillRect(0, 0, map.widthInPixels * 2, map.heightInPixels * 2);
-
-        //set depths
-        this.fov.setDepth(1);
-        this.layerWalls.setDepth(1);
-        this.layerDoor.setDepth(1);
-        this.graphics.setDepth(3);
-    }
-
-    setRaycasting() {
-        this.raycaster = this.raycasterPlugin.createRaycaster({
-            autoUpdate: false,
-        });
-        
-        this.raycaster.mapGameObjects(this.layerWalls, true, {
-            collisionTiles: this.layerWalls.layer.collideIndexes
-        });
-
-        // this.raycaster.mapGameObjects(this.layerDoor, true, {
-        //     collisionTiles: this.layerDoor.layer.collideIndexes
-        // });
-
-        this.ray = this.raycaster.createRay();
-        this.updateRays()
-    }
-
-    setPlayerMovement() {
-        this.input.keyboard!.on('keydown-LEFT', () => {
-            if (this.isTileCollider('left')) return;
-            this.player.x -= this.tileSize
-            this.updateRays()
-            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
-        })
-        this.input.keyboard!.on('keydown-RIGHT', () => {
-            if (this.isTileCollider('right')) return;
-            this.player.x += this.tileSize
-            this.updateRays()
-            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
-        })
-        this.input.keyboard!.on('keydown-UP', () => {
-            if (this.isTileCollider('up')) return;
-            this.player.y -= this.tileSize
-            this.updateRays()
-            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
-        })
-        this.input.keyboard!.on('keydown-DOWN', () => {
-            if (this.isTileCollider('down')) return;
-            this.player.y += this.tileSize
-            this.updateRays()
-            EventBus.emit('playerMovement', { x: this.player.x, y: this.player.y })
-        })
-    }
-
-    updateRays() {
-        this.ray.setOrigin(this.player.x, this.player.y)
-        this.rayIntersections = this.ray.castCircle()
-        this.drawRays()
-    }
-
-    drawRays() {
-        this.graphics.clear();
-        this.maskGraphics.clear();
-        this.maskGraphics.fillPoints(this.rayIntersections);
-
-        // draw rays
-        for (const intersection of this.rayIntersections) {
-        // @ts-ignore
-        this.graphics.strokeLineShape({
-            x1: this.ray.origin.x,
-            y1: this.ray.origin.y,
-            x2: intersection.x,
-            y2: intersection.y
-        });
-
-        this.graphics.fillStyle(0xff00ff);
-        //draw ray origin
-        this.graphics.fillPoint(this.ray.origin.x, this.ray.origin.y, 3);
-    }
-        
-}
 
     loadLayers(map: Phaser.Tilemaps.Tilemap) {
         const tiles_wall_wooden = map.addTilesetImage('wall_wooden', "wall_wooden");
@@ -236,37 +68,9 @@ export class Home extends Scene
         this.layerStockpiles?.setScale(2)
         this.layerItems?.setScale(2)
         this.layerDoor?.setScale(2).setCollisionByExclusion([-1])
-    }
 
-    isTileCollider(direction: 'left'|'right'|'down'|'up') {
-        const getTilePosition = (): number[] => {
-            if (direction === 'left') {
-                return [this.player.x - this.tileSize, this.player.y]
-            }
-            if (direction === 'right') {
-                return [this.player.x + this.tileSize, this.player.y]
-            }
-            if (direction === 'down') {
-                return [this.player.x, this.player.y + this.tileSize]
-            }
-            if (direction === 'up') {
-                return [this.player.x, this.player.y - this.tileSize]
-            }
-            return [0,0]
-        }
-
-        // Get position of the tile where player will move to soon
-        const [x, y] = getTilePosition()
-
-        // Check if the tile is present in any of the "collission" layers
-        const collissionLayers = [this.layerWalls, this.layerFurniture]
-        const collissionTiles = collissionLayers.map(layer => 
-            layer.getTileAtWorldXY(x, y, true)?.index ?? -1
-        )
-        
-        // If yes, the tile is impassable and we have to block player from moving through it
-        if (collissionTiles.some(index => index !== -1)) return true
-        return false
+        this.collisionLayers = [this.layerWalls, this.layerFurniture]
+        this.rayCollisionLayers = [this.layerWalls]
     }
 }
 
